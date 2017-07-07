2063b97624bdc69cb3aa77c6d2664251b3f936c3
sj.kwon_sh <ksjksjgg@gmail.com>
2017-07-07 23:23:38 +0900

    term2, P2 , lesson folder

---
+++ Term2_Robotics_Sensor_Fusion/02_Project_Unscented_Kalman_Filter/99_Lessons/lesson_code.cpp
@@ -0,0 +1,241 @@
+// Sigma Point Prediction Assignment 1
+// Goal : Get Xsig from Xsig_aug
+// ukf.cpp
+
+/*******************************************************************************
+* Programming assignment functions:
+*******************************************************************************/
+
+void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
+
+  //set state dimension
+  int n_x = 5;
+
+  //set augmented dimension
+  int n_aug = 7;
+
+  //create example sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+     Xsig_aug <<
+    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
+      1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
+    2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
+    0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
+    0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
+         0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
+         0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
+
+  //create matrix with predicted sigma points as columns
+  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+
+  double delta_t = 0.1; //time diff in sec
+/*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+
+  //predict sigma points
+  for (int i = 0; i< 2*n_aug+1; i++)
+  {
+    //extract values for better readability
+    double p_x = Xsig_aug(0,i);
+    double p_y = Xsig_aug(1,i);
+    double v = Xsig_aug(2,i);
+    double yaw = Xsig_aug(3,i);
+    double yawd = Xsig_aug(4,i);
+    double nu_a = Xsig_aug(5,i);
+    double nu_yawdd = Xsig_aug(6,i);
+
+    //predicted state values
+    double px_p, py_p;
+
+    //avoid division by zero
+    if (fabs(yawd) > 0.001) {
+        px_p = p_x + v/yawd * ( sin (yaw + yawd*delta_t) - sin(yaw));
+        py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+    }
+    else {
+        px_p = p_x + v*delta_t*cos(yaw);
+        py_p = p_y + v*delta_t*sin(yaw);
+    }
+
+    double v_p = v;
+    double yaw_p = yaw + yawd*delta_t;
+    double yawd_p = yawd;
+
+    //add noise
+    px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+    py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+    v_p = v_p + nu_a*delta_t;
+
+    yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+    yawd_p = yawd_p + nu_yawdd*delta_t;
+
+    //write predicted sigma point into right column
+    Xsig_pred(0,i) = px_p;
+    Xsig_pred(1,i) = py_p;
+    Xsig_pred(2,i) = v_p;
+    Xsig_pred(3,i) = yaw_p;
+    Xsig_pred(4,i) = yawd_p;
+  }
+
+/*******************************************************************************
+ * Student part end
+ ******************************************************************************/
+
+  //print result
+  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
+
+  //write result
+  *Xsig_out = Xsig_pred; // Note Xsig_out(5,15) in main.cpp
+
+}
+
+
+// 23. Predicted Mean and Covariance Assignment 1
+// Goal : estimate  new x, P from Xsig_pred
+// ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
+
+/*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+
+  // set weights
+  double weight_0 = lambda/(lambda+n_aug);
+  weights(0) = weight_0;
+  for (int i=1; i<2*n_aug+1; i++) {  //2n+1 weights
+    double weight = 0.5/(n_aug+lambda);
+    weights(i) = weight;
+  }
+
+  //predicted state mean
+  x.fill(0.0);
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //iterate over sigma points
+    x = x+ weights(i) * Xsig_pred.col(i);
+  }
+
+  //predicted state covariance matrix
+  P.fill(0.0);
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //iterate over sigma points
+
+    // state difference
+    VectorXd x_diff = Xsig_pred.col(i) - x;
+
+    //angle normalization
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+    //Note : angle normalization
+    // x_diff(3)=atan2(sin(x_diff(3)), cos(x_diff(3)));
+
+    P = P + weights(i) * x_diff * x_diff.transpose() ;
+  }
+
+
+/*******************************************************************************
+ * Student part end
+ ******************************************************************************/
+
+
+
+//26.Predict Radar Measurement Assignment 1
+// transform Xsig_pred to Zsig
+/*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+
+  //transform sigma points into measurement space
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //2n+1 simga points
+
+    // extract values for better readibility
+    double p_x = Xsig_pred(0,i);
+    double p_y = Xsig_pred(1,i);
+    double v  = Xsig_pred(2,i);
+    double yaw = Xsig_pred(3,i);
+
+    double v1 = cos(yaw)*v;
+    double v2 = sin(yaw)*v;
+
+    // measurement model
+    Zsig(0,i) = sqrt(p_x*p_x + p_y*p_y);                        //r
+    Zsig(1,i) = atan2(p_y,p_x);                                 //phi
+    Zsig(2,i) = (p_x*v1 + p_y*v2 ) / sqrt(p_x*p_x + p_y*p_y);   //r_dot
+  }
+
+  //mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
+  z_pred.fill(0.0);
+  for (int i=0; i < 2*n_aug+1; i++) {
+      z_pred = z_pred + weights(i) * Zsig.col(i);
+  }
+
+  //measurement covariance matrix S
+  MatrixXd S = MatrixXd(n_z,n_z);
+  S.fill(0.0);
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //2n+1 simga points
+    //residual
+    VectorXd z_diff = Zsig.col(i) - z_pred;
+
+    //angle normalization
+    while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+    while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+    S = S + weights(i) * z_diff * z_diff.transpose();
+  }
+
+  //add measurement noise covariance matrix
+  MatrixXd R = MatrixXd(n_z,n_z);
+  R <<    std_radr*std_radr, 0, 0,
+          0, std_radphi*std_radphi, 0,
+          0, 0,std_radrd*std_radrd;
+  S = S + R;
+
+
+/*******************************************************************************
+ * Student part end
+ ******************************************************************************/
+
+
+ // UKF Update Assignment 1
+ // Calculate nest x , P
+
+
+ /*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+
+ //calculate cross correlation matrix
+ Tc.fill(0.0);
+ for (int i = 0; i < 2 * n_aug + 1; i++) {  //2n+1 simga points
+
+   //residual
+   VectorXd z_diff = Zsig.col(i) - z_pred;
+   //angle normalization
+   while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+   while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+   // state difference
+   VectorXd x_diff = Xsig_pred.col(i) - x;
+   //angle normalization
+   while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+   while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+   Tc = Tc + weights(i) * x_diff * z_diff.transpose();
+ }
+
+ //Kalman gain K;
+ MatrixXd K = Tc * S.inverse();
+
+ //residual
+ VectorXd z_diff = z - z_pred;
+
+ //angle normalization
+ while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+ while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+ //update state mean and covariance matrix
+ x = x + K * z_diff;
+ P = P - K*S*K.transpose();
+
+/*******************************************************************************
+ * Student part end
+ ******************************************************************************/
